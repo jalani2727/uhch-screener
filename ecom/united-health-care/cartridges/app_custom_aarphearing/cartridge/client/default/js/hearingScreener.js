b71b2147a9f0255ec4a0b8fc6472a76173e62ff1
jalani2727 <jalani2727@gmail.com>
2023-07-13 17:35:57 -0400

    Update screener content structure - Add next/previous functionality

--- ecom/united-health-care/cartridges/app_custom_aarphearing/cartridge/client/default/js/hearingScreener.js
+++ ecom/united-health-care/cartridges/app_custom_aarphearing/cartridge/client/default/js/hearingScreener.js
@@ -3,6 +3,8 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
+const fieldsets = $('.screener-form-container fieldset');
+
 $(".next").click(function () {
     if (animating) return false;
     animating = true;
@@ -10,37 +12,42 @@ $(".next").click(function () {
     current_fs = $(this).parent();
     next_fs = $(this).parent().next();
 
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({
-        opacity: 0
-    }, {
-        step: function (now, mx) {
-            //as the opacity of current_fs reduces to 0 - stored in "now"
-            //1. scale current_fs down to 80%
-            scale = 1 - (1 - now) * 0.2;
-            //2. bring next_fs from the right(50%)
-            left = (now * 50) + "%";
-            //3. increase opacity of next_fs to 1 as it moves in
-            opacity = 1 - now;
-            current_fs.css({
-                'transform': 'scale(' + scale + ')',
-                'position': 'absolute'
-            });
-            next_fs.css({
-                'left': left,
-                'opacity': opacity
-            });
-        },
-        duration: 800,
-        complete: function () {
+    // Iterate through fieldsets
+    fieldsets.each(function(index, fs) {
+        // Update CSS on current fieldset
+        if (fs == current_fs.get(0)){
             current_fs.hide();
-            animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
+
+            current_fs.animate({
+                opacity: 0
+            }, {
+                step: function (now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale current_fs down to 80%
+                    scale = 1.2 - (1 - now) * 0.2;
+                    //2. bring next_fs from the right(50%)
+                    left = (now * 50) + "%";
+                    //3. increase opacity of next_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    current_fs.css({
+                        'transform': 'scale(' + scale + ')',
+                    });
+                    next_fs.css({
+                        'transform': 'scale(' + scale + ')',
+                        'opacity': opacity
+                    });
+                    //show the next fieldset
+                    next_fs.show();
+                },
+                duration: 800,
+                complete: function () {
+                    animating = false;
+                },
+            });
+        }
     });
+
+
 });
 
 $(".previous").click(function () {
@@ -50,12 +57,12 @@ $(".previous").click(function () {
     current_fs = $(this).parent();
     previous_fs = $(this).parent().prev();
 
-    //de-activate current step on progressbar
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+    //TODO: Change current step on progressbar
+
 
     //show the previous fieldset
     previous_fs.show();
-    //hide the current fieldset with style
+    //hide the current fieldset
     current_fs.animate({
         opacity: 0
     }, {
@@ -68,19 +75,23 @@ $(".previous").click(function () {
             //3. increase opacity of previous_fs to 1 as it moves in
             opacity = 1 - now;
             current_fs.css({
-                'left': left
+                // 'left': left
             });
             previous_fs.css({
                 'transform': 'scale(' + scale + ')',
+                'position': 'relative',
                 'opacity': opacity
             });
+            current_fs.hide();
         },
         duration: 800,
         complete: function () {
-            current_fs.hide();
+            // current_fs.hide();
             animating = false;
         },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
     });
 });
+
+$(".submit").click(function () {
+    return false;
+})
--- ecom/united-health-care/cartridges/app_custom_uhc_core/cartridge/client/default/js/homePage.js
+++ ecom/united-health-care/cartridges/app_custom_uhc_core/cartridge/client/default/js/homePage.js
@@ -2,6 +2,7 @@
 
 var processInclude = require('base/util');
 require('base/productTile');
+require('../../../../../app_custom_aarphearing/cartridge/client/default/js/hearingScreener');
 
 $(document).ready(function () {
     processInclude(require('./components/registrationSuccessModal'));
